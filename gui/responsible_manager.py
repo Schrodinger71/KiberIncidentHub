16e0edf8d70fc0df41e9c88d00bf3c6e885cfaaf
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2025-06-15 14:59:53 +0300

    - В Ответственных выбор организации реализован через CTkComboBox

--- gui/responsible_manager.py
+++ gui/responsible_manager.py
@@ -25,13 +25,13 @@ class ResponsibleManager(ctk.CTkFrame):
         self.email_entry = ctk.CTkEntry(self, placeholder_text="Email")
         self.email_entry.grid(row=0, column=2, padx=5, pady=5, sticky="ew")
 
-        # Комбо для выбора организации по названию
+        # CTkComboBox для выбора организации
         self.org_var = ctk.StringVar()
         org_names = [org[1] for org in self.organizations]
-        self.org_combobox = ttk.Combobox(self, textvariable=self.org_var, values=org_names, state="readonly")
+        self.org_combobox = ctk.CTkComboBox(self, variable=self.org_var, values=org_names, state="readonly")
         self.org_combobox.grid(row=0, column=3, padx=5, pady=5, sticky="ew")
         if org_names:
-            self.org_combobox.current(0)
+            self.org_combobox.set(org_names[0])
 
         self.add_button = ctk.CTkButton(self, text="Добавить", command=self._add_responsible)
         self.add_button.grid(row=1, column=0, padx=5, pady=5)
@@ -85,7 +85,6 @@ class ResponsibleManager(ctk.CTkFrame):
         self._load_responsibles()
         self.after(10000, self._start_auto_refresh)
 
-
     def _load_responsibles(self):
         for row in self.tree.get_children():
             self.tree.delete(row)
@@ -108,7 +107,7 @@ class ResponsibleManager(ctk.CTkFrame):
             self.email_entry.delete(0, "end")
             self.email_entry.insert(0, values[2] if values[2] != "-" else "")
             # Выбрать организацию в комбобоксе
-            if values[3] in self.org_combobox['values']:
+            if values[3] in self.org_combobox.cget("values"):
                 self.org_combobox.set(values[3])
             else:
                 self.org_combobox.set("")
